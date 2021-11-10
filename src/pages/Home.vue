a868e0b1b68a8353730a9ce385ffd26b8edf3cef
manudurgoni <emmanuel@carillon.studio>
2021-11-10 15:48:04 +0100

    up

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <router-link class="block my-6 text-lg" v-for="post in posts" :key="post.id" :to="`/news/${post.id}`">{{ post.text}}</router-link>
 
-    <button class="">Voir plus</button>
+    <button class="text-white bg-gray-800 p-4 my-10" @click="showMore" v-if="!isLast">Voir plus</button>
   </div>
 </template>
 
@@ -18,14 +18,34 @@ export default {
       total: 0,
     }
   },
-  async created() {
-    const json = await API.getArticles({
-      limit: this.limit,
-      page: this.page
-    })
 
-    this.posts = json.data
-    this.total = json.total
+  computed: {
+    isLast() {
+      const lastPage = Math.round(this.total / this.limit)
+      console.log(lastPage, this.page)
+      return this.page >= lastPage
+    }
+  },
+
+  async created() {
+    this.getData()
+  },
+
+  methods: {
+    showMore() {
+      this.page++
+      this.getData()
+    },
+
+    async getData() {
+      const json = await API.getArticles({
+        limit: this.limit,
+        page: this.page
+      })
+
+      this.posts.push(...json.data)
+      this.total = json.total
+    }
   }
 }
 </script>
